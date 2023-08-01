0d13a065d1d54d1c226005853ac17fc4b19c4935
The Fox <lisieckikamil93@gmail.com>
2023-08-01 21:34:13 +0200

    ..

--- src/global.js
+++ src/global.js
@@ -4,7 +4,7 @@ export const GlobalContext = createContext();
 
 export const GlobalProvider = ({ children }) => {
   const [filtredRegion, setFiltredRegion] = useState([]);
-  const [swich, setSwich] = useState(false);
+  const [swich, setSwich] = useState(true);
 
   return (
     <GlobalContext.Provider
